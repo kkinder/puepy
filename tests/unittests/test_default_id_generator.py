d1f7e1b13fa6202fb1d3716362eb6d3293ac7a54
Ken Kinder <ken+github@kkinder.com>
2024-09-07 17:14:19 +0100

    Switch test_default_id_generator.py to pytest
    re #61

--- tests/unittests/test_default_id_generator.py
+++ tests/unittests/test_default_id_generator.py
@@ -1,25 +1,23 @@
-import unittest
 import itertools
+import pytest
 from puepy.application import DefaultIdGenerator
 
 
-class TestDefaultIdGenerator(unittest.TestCase):
-    def setUp(self):
-        self.id_generator = DefaultIdGenerator("test")
+class TestDefaultIdGenerator:
 
-    def test_init(self):
-        self.assertEqual(self.id_generator.prefix, "test")
-        self.assertIsInstance(self.id_generator.counter, itertools.count)
+    @pytest.fixture
+    def id_generator(self):
+        return DefaultIdGenerator("test")
+
+    def test_init(self, id_generator):
+        assert id_generator.prefix == "test"
+        assert isinstance(id_generator.counter, itertools.count)
 
     def test_int_to_base36(self):
         num = 10
-        self.assertEqual(DefaultIdGenerator._int_to_base36(num), "a")
+        assert DefaultIdGenerator._int_to_base36(num) == "a"
 
-    def test_call(self):
-        ids = [self.id_generator.get_id_for_element(None) for _ in range(100)]
-        self.assertEqual(ids[0], "test0")
-        self.assertEqual(ids[99], "test2r")
-
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_call(self, id_generator):
+        ids = [id_generator.get_id_for_element(None) for _ in range(100)]
+        assert ids[0] == "test0"
+        assert ids[99] == "test2r"
