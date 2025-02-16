97c0ebb42a531bf382640615fb94e31bca613bb3
Ken Kinder <ken+github@kkinder.com>
2025-02-16 12:21:02 +0000

    Stop using kwargs to generate refs
    Previously, to decide whether to re-use or create new components/tags, PuePy would include a "ref hash" that included kwargs (props) passed to the component.
    
    That was foolish. Props change, but the component should still be the same.

--- puepy/core.py
+++ puepy/core.py
@@ -895,10 +895,7 @@ class Builder:
             raise Exception("t.generate_tag called without a context")
 
         # Determine ref value
-        ref_part = "__" + (
-            f"{parent.ref}.{tag_name}{len(parent.children) + 1}_"
-            + (binascii.hexlify(hashlib.sha256(str(kwargs).encode()).digest()))[:8].decode("utf8")
-        ).lstrip("_")
+        ref_part = "__" + (f"{parent.ref}.{tag_name}_{len(parent.children) + 1}").lstrip("_")
 
         ref = kwargs.pop("ref", ref_part)
 
